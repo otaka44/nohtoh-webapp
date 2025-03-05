27d1ee7d6aeaccaa2d8e3a533181e864579a9806
otaka44 <taka44.ono@icloud.com>
2025-03-06 00:09:58 +0900

    fontは読み込みに時間がかかるため削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
 import brainLumpLogo from "./assets/brain-lump-logo.svg";
 import "./App.css";
 import DialogButton from "@/components/dialog-button";

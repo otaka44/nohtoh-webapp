70e3bb57db100c7e458be0a09a6af46084abe25d
otaka44 <taka44.ono@icloud.com>
2025-03-06 01:00:30 +0900

    色変更

--- src/lib/calc_fatigue_level.ts
+++ src/lib/calc_fatigue_level.ts
@@ -25,14 +25,12 @@ class BrainFatigueLevel {
     const colors: {
       [key: number]: { red: number; green: number; blue: number };
     } = {
-      0: { red: 216, green: 59, blue: 68 }, // #D83B44
-      1: { red: 235, green: 157, blue: 95 }, // #EB9D5F
-      2: { red: 226, green: 191, blue: 106 }, // #E2BF6A
-      3: { red: 161, green: 193, blue: 88 }, // #A1C158
-      4: { red: 42, green: 151, blue: 137 }, // #2A9789
-      5: { red: 68, green: 119, blue: 150 }, // #447796
-      6: { red: 27, green: 52, blue: 85 }, // #1B3455
-      7: { red: 106, green: 87, blue: 119 }, // #1B3455（異なる色合い）
+      0: { red: 255, green: 1, blue: 1 }, // #ff0101
+      1: { red: 255, green: 20, blue: 147 }, // #FF1493
+      2: { red: 255, green: 216, blue: 0 }, // #FFD800
+      3: { red: 1, green: 255, blue: 1 }, // #01ff01
+      4: { red: 1, green: 1, blue: 255 }, // #0101ff
+      5: { red: 255, green: 255, blue: 255 }, // #ffffff（異なる色合い）
     };
     return colors[n];
   }
@@ -48,11 +46,7 @@ class BrainFatigueLevel {
       -alpha * this.tWake - beta * this.tWork - gamma * this.tScreenTime;
     const result = 1 - Math.exp(exponent);
 
-    if (result <= 0.4) {
-      return this.getColor(7);
-    } else if (result < 0.475) {
-      return this.getColor(6);
-    } else if (result < 0.55) {
+    if (result <= 0.625) {
       return this.getColor(5);
     } else if (result < 0.625) {
       return this.getColor(4);
@@ -76,11 +70,7 @@ class BrainFatigueLevel {
     const k = 3.952847075210474e-10;
     const fl = k * this.tMusic * Math.pow(10, this.soundLevel / 10);
 
-    if (fl <= 0.12) {
-      return this.getColor(7);
-    } else if (fl < 0.24) {
-      return this.getColor(6);
-    } else if (fl < 0.36) {
+    if (fl <= 0.36) {
       return this.getColor(5);
     } else if (fl < 0.48) {
       return this.getColor(4);
@@ -106,13 +96,9 @@ class BrainFatigueLevel {
     const fl =
       (k / (luminance + 1)) * (this.tScreenTime / Math.pow(distanceScreen, 2));
 
-    if (fl <= 0.12) {
-      return this.getColor(7);
-    } else if (fl < 0.24) {
-      return this.getColor(6);
-    } else if (fl < 0.36) {
+    if (fl <= 0.1) {
       return this.getColor(5);
-    } else if (fl < 0.48) {
+    } else if (fl < 0.4) {
       return this.getColor(4);
     } else if (fl < 0.6) {
       return this.getColor(3);
@@ -120,7 +106,7 @@ class BrainFatigueLevel {
       return this.getColor(2);
     } else if (fl < 0.84) {
       return this.getColor(1);
-    } else if (fl >= 1.0) {
+    } else if (fl >= 0.72) {
       return this.getColor(0);
     }
     return this.getColor(4);
